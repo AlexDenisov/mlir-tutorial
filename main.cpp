9d7b58f30a4f2ab62cd3990d74462aebe2aff67c
AlexDenisov <alex@lowlevelbits.org>
2022-08-01 00:03:33 +0200

    Add MRuby

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <mruby.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  auto mrb = mrb_open();
+  auto top = mrb_top_self(mrb);
+  auto n = mrb_fixnum_value(42);
+  mrb_funcall(mrb, top, "puts", 1, n);
+  mrb_close(mrb);
+  return 0;
 }
